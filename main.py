88247448fb031bdb6168924fafb2c8eae21b346b
TALightTest <142103438+TALightTest@users.noreply.github.com>
2023-09-10 20:15:23 +0200

    Upload from TALight Desktop --> [10/09/2023 20:15:23]

--- main.py
+++ main.py
@@ -1,11 +1,1 @@
-#!bin/python3
-
-cnt = int(await input())
-for i in range(cnt):
-    line = await input()
-    #print("line:", line)
-    nums = line.split(" ")
-    a = int(nums[0])
-    b = int(nums[1])
-    print(a+b)
-    
+print("Hello World!")
